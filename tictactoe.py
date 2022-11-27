740c1b61a502979ede746c0a57a98bd9a3d60ab6
Alin Gavriliu <alin_gavriliu@yahoo.com>
2022-11-27 14:40:49 +0200

    game init

---
+++ tictactoe.py
@@ -0,0 +1,42 @@
+import sys
+import pygame
+
+from constants import *
+
+# Initialize pygame
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tic Tac Toe")
+screen.fill(BG_COLOR)
+
+
+class Game:
+    def __init__(self):
+        self.show_lines()
+        pass
+
+    def show_lines(self):
+        # Vertical lines
+        pygame.draw.line(screen, LINE_COLOR, (SQUARE_SIZE, 0), (SQUARE_SIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (WIDTH - SQUARE_SIZE, 0), (WIDTH - SQUARE_SIZE, HEIGHT), LINE_WIDTH)
+
+        # Horizontal lines
+        pygame.draw.line(screen, LINE_COLOR, (0, SQUARE_SIZE), (WIDTH, SQUARE_SIZE), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT - SQUARE_SIZE), (WIDTH, HEIGHT - SQUARE_SIZE), LINE_WIDTH)
+
+    def run(self):
+        pass
+
+# Main loop
+def main():
+    game = Game()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+main()
