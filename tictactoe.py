4ffe4b4bc6d0ebb2f59a5934db62ca5858e7e5c9
Alin Gavriliu <alin_gavriliu@yahoo.com>
2022-11-27 15:07:13 +0200

    added X & 0 figures on-click

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import numpy as np
 
 from constants import *
 
@@ -10,8 +11,21 @@ pygame.display.set_caption("Tic Tac Toe")
 screen.fill(BG_COLOR)
 
 
+class Board:
+    def __init__(self):
+        self.squares = np.zeros((ROWS, COLS))
+
+    def mark_square(self, row, col, player):
+        self.squares[row][col] = player
+
+    def is_empty_square(self, row, col):
+        return self.squares[row][col] == 0
+
+
 class Game:
     def __init__(self):
+        self.board = Board()
+        self.player = 1
         self.show_lines()
         pass
 
@@ -24,18 +38,48 @@ class Game:
         pygame.draw.line(screen, LINE_COLOR, (0, SQUARE_SIZE), (WIDTH, SQUARE_SIZE), LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT - SQUARE_SIZE), (WIDTH, HEIGHT - SQUARE_SIZE), LINE_WIDTH)
 
+    def next_turn(self):
+        self.player = self.player % 2 + 1
+
+    def draw_fig(self, row, col):
+        if self.player == 1:
+            # Draw X
+            start_desc = (col * SQUARE_SIZE + OFFSET, row * SQUARE_SIZE + OFFSET)
+            end_desc = ((col + 1) * SQUARE_SIZE - OFFSET, (row + 1) * SQUARE_SIZE - OFFSET)
+            pygame.draw.line(screen, CROSS_COLOR, start_desc, end_desc, CROSS_WIDTH)
+
+            start_asc = ((col + 1) * SQUARE_SIZE - OFFSET, row * SQUARE_SIZE + OFFSET)
+            end_asc = (col * SQUARE_SIZE + OFFSET, (row + 1) * SQUARE_SIZE - OFFSET)
+            pygame.draw.line(screen, CROSS_COLOR, start_asc, end_asc, CROSS_WIDTH)
+        elif self.player == 2:
+            # Draw O
+            center = (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2)
+            pygame.draw.circle(screen, CIRCLE_COLOR, center, RADIUS, CIRCLE_WIDTH)
+
+
     def run(self):
         pass
 
-# Main loop
 def main():
     game = Game()
+    board = game.board
 
+    # Main loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos
+                row = pos[1] // SQUARE_SIZE
+                col = pos[0] // SQUARE_SIZE
+
+                if board.is_empty_square(row, col):
+                    board.mark_square(row, col, game.player)
+                    game.draw_fig(row, col)
+                    game.next_turn()
 
         pygame.display.update()
 
